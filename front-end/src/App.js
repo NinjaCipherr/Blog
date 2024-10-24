de26f0fa0dafadc99385360a020bbcd81b8152cd
ThienOne <thienone1628.io@gmail.com>
2024-10-24 22:21:14 +0900

    oke

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,6 +8,12 @@ function App() {
         <a href="" className="logo">
           My Blog
         </a>
+        <nav>
+          <a href="" className="">
+              Login
+          </a>
+          <a href="">Register</a>
+        </nav>
       </header>
     </main>
   );
