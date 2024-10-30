cc5e04075596d96bce267be1c4bc022dc2472205
NinjaCipher <thienone1628.io@gmail.com>
2024-10-30 18:53:25 +0900

    oke

--- back-end/entity/User.js
+++ back-end/entity/User.js
@@ -1,3 +1,9 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
-const UserSchema = new Schema({});
+const UserSchema = new Schema({
+  username: { type: String, required: true, min: 4, unique: true },
+  password: { type: String, required: true },
+});
+const UserEntity = mongoose.model("User", UserSchema);
+
+module.exports = UserEntity;
--- back-end/index.js
+++ back-end/index.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+
+const User = require("./entity/User");
+const mongoose = require("mongoose");
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect(
+  "mongodb+srv://vanthienpham734:Aa28121998@cluster0.rp80y.mongodb.net/"
+);
 app.post("/register", (req, res) => {
   const { username, password } = req.body;
+  User.create();
   res.json({ requestData: { username, password } });
 });
 app.listen(4000);
